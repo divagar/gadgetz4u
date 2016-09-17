593064764b3e4ffe46c6f531df10608043490630
Div <m.divagar@gmail.com>
2016-09-17 16:12:39 +0530

    Angular and Angular CLI Upgrade - App src and general env changes.
    Signed-off-by: Div <m.divagar@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,12 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { Title } from '@angular/platform-browser';
+import './polyfills.ts';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { Gadgetz4uAppComponent, environment } from './app/';
-import { FIREBASE_PROVIDERS, defaultFirebase, AngularFire } from 'angularfire2';
+import { environment } from './environments/environment';
+import { Gadgetz4uModule } from './app/gadgetz4u.module';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(Gadgetz4uAppComponent,[
-  Title,
-  FIREBASE_PROVIDERS,
-  defaultFirebase({
-    apiKey: "AIzaSyAhazzDCJPH0ADelG3bZ_HJyy8PMKlmfiY",
-    authDomain: "gadgetz-3396f.firebaseapp.com",
-    databaseURL: "https://gadgetz-3396f.firebaseio.com",
-    storageBucket: "gadgetz-3396f.appspot.com",
-  })
-]);
+platformBrowserDynamic().bootstrapModule(Gadgetz4uModule);
